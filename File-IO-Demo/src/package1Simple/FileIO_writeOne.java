325fcd30a57353fec104168343a33bb1860f393a
rkjc <richardkjcross@gmail.com>
2022-01-31 12:43:51 -0800

    quick code update

--- File-IO-Demo/src/package1Simple/FileIO_writeOne.java
+++ File-IO-Demo/src/package1Simple/FileIO_writeOne.java
@@ -4,20 +4,18 @@ import java.io.File;
 import java.io.PrintWriter;
 
 public class FileIO_writeOne {
-
 	public static void main(String[] args) {
 
 		try {
 			// create a File instance object that connects to the named file and path
-			File aFile = new File("writeOneFile.txt"); // use (pathString , true) to append to the file
+			// use File(pathString , true) to append to the file, default is to overwrite
+			File aFile = new File("writeOneFile.txt");
 
 			// create a PrintWriter for easy interaction with the FileWriter instance object
 			PrintWriter prtout = new PrintWriter(aFile);
 
 			// print this text into the file
-			prtout.println("hello text to file writing world!");
-			
-			System.out.println(aFile.getAbsolutePath());
+			prtout.println("hello text to file writing world!");		
 					
 			// make sure that all pending print operations were sent to the file before closing
 			prtout.flush();
