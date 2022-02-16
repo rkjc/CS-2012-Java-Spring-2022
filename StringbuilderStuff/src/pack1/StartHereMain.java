677bce0152b573fdfff5e5b499a871d063fff2f9
rkjc <richardkjcross@gmail.com>
2022-02-16 12:55:38 -0800

    quick code update

---
+++ StringbuilderStuff/src/pack1/StartHereMain.java
@@ -0,0 +1,23 @@
+package pack1;
+
+public class StartHereMain {
+
+	public static void main(String[] args) {
+		
+		String message = new String("Welcome to Java");
+		System.out.println(message);	
+		
+		String luke = "happy joy";
+		
+		message = "text has been changed";
+		System.out.println(message);
+		
+		
+		int bob = 78;
+		Integer joe = 99;
+		
+		
+
+	}
+
+}
