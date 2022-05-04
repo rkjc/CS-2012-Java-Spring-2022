d309fb95b12c17684721758825ce379b193d56a6
rkjc <richardkjcross@gmail.com>
2022-05-04 13:30:34 -0700

    save changes

--- 10-GUI_javaFXbasics/src/mouseStuff/MouseDrawingMain.java
+++ 10-GUI_javaFXbasics/src/mouseStuff/MouseDrawingMain.java
@@ -8,9 +8,11 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -24,15 +26,19 @@ public class MouseDrawingMain extends Application {
 	double mouseDownY = 0.0;
 	double mouseUpY = 9.0;
 	
+	Pane drawPane = new Pane();
+	
 	@Override
 	public void start(Stage outStageObject) {
 		outStageObject.setTitle("Drawing Shapes");
 		//creating group object for adding canvas object to it
 		Group groupObject = new Group();
-		// creating canvas object
-		Canvas canvasRef = new Canvas(400, 300);
-		// creating GraphicsContext for drawing the image
-		GraphicsContext graphicContext = canvasRef.getGraphicsContext2D();
+		
+
+//		// creating canvas object
+//		Canvas canvasRef = new Canvas(400, 300);
+//		// creating GraphicsContext for drawing the image
+//		GraphicsContext graphicContext = canvasRef.getGraphicsContext2D();
 		
 		EventHandler<MouseEvent> mousClik = new EventHandler<MouseEvent>() {
 			@Override
@@ -65,36 +71,43 @@ public class MouseDrawingMain extends Application {
 			public void handle(MouseEvent myz) {
 				mouseUpX = myz.getX();
 				mouseUpY = myz.getX();	
-				drawLine(graphicContext);
+				//drawLine(graphicContext);
 			}
 		};
 		
 		
+//		// setting the color to shapes
+//		graphicContext.setFill(Color.DARKKHAKI);
+//
+//		// setting the shadow color to the shape
+//		graphicContext.setStroke(Color.DARKVIOLET);
 
-
-
-		// setting the color to shapes
-		graphicContext.setFill(Color.DARKKHAKI);
-
-		// setting the shadow color to the shape
-		graphicContext.setStroke(Color.DARKVIOLET);
-
-		// setting line width
-		graphicContext.setLineWidth(6);
+//		// setting line width
+//		graphicContext.setLineWidth(6);
 		
-		// adding shapes
+//		// adding shapes
+//		graphicContext.fillOval(11, 61, 31, 31);
+//		graphicContext.fillRoundRect(111, 61, 31, 31, 11, 11);
+//		graphicContext.strokeRoundRect(161, 61, 31, 31, 11, 11);
+//		graphicContext.fillArc(110, 111, 31, 31, 46, 241, ArcType.ROUND);
+//		graphicContext.strokeArc(111, 111, 31, 31, 46, 241, ArcType.ROUND);
 
-		//graphicContext.fillOval(11, 61, 31, 31);
-
-		graphicContext.fillRoundRect(111, 61, 31, 31, 11, 11);
-
-		graphicContext.strokeRoundRect(161, 61, 31, 31, 11, 11);
-
-		graphicContext.fillArc(110, 111, 31, 31, 46, 241, ArcType.ROUND);
-
-		graphicContext.strokeArc(111, 111, 31, 31, 46, 241, ArcType.ROUND);
-
+	    Circle circle = new Circle();
+	    circle.setCenterX(100);
+	    circle.setCenterY(100);
+	    circle.setRadius(50);
+	    circle.setStroke(Color.BLACK);
+	    circle.setFill(null);
 		
+		Line line1 = new Line(10, 10, 50, 100);
+//	    line1.endXProperty().bind(widthProperty().subtract(10));
+//	    line1.endYProperty().bind(heightProperty().subtract(10));
+	    line1.setStrokeWidth(5);
+	    line1.setStroke(Color.GREEN);
+	    drawPane.getChildren().add(line1);
+		drawPane.getChildren().add(circle);
+	    
+		//groupObject.getChildren().add(myCircle);
 		
 		VBox vbox1 = new VBox(10);
 		HBox hbox1 = new HBox(10);
@@ -102,18 +115,25 @@ public class MouseDrawingMain extends Application {
 		hbox1.getChildren().add(txt1);
 		hbox1.getChildren().add(txt2);
 
-		groupObject.getChildren().add(canvasRef);
+		//groupObject.getChildren().add(canvasRef);
 
-		groupObject.setOnMouseClicked(mousClik);
-		groupObject.setOnMouseMoved(mousLoc);
-		groupObject.setOnMousePressed(mousPress);
-		groupObject.setOnMouseReleased(mousRels);
+//		groupObject.setOnMouseClicked(mousClik);
+//		groupObject.setOnMouseMoved(mousLoc);
+//		groupObject.setOnMousePressed(mousPress);
+//		groupObject.setOnMouseReleased(mousRels);
+		
+		drawPane.resize(500, 500);
+		
+		drawPane.setOnMouseClicked(mousClik);
+		drawPane.setOnMouseMoved(mousLoc);
+		drawPane.setOnMousePressed(mousPress);
+		drawPane.setOnMouseReleased(mousRels);
 
-		vbox1.getChildren().add(groupObject);
+		vbox1.getChildren().add(drawPane);
 		vbox1.getChildren().add(hbox1);
 		
 		// creating scene object
-		Scene sceneObject = new Scene(vbox1);
+		Scene sceneObject = new Scene(vbox1, 500, 500);
 		outStageObject.setScene(sceneObject);
 		outStageObject.show();
 	}
@@ -121,6 +141,7 @@ public class MouseDrawingMain extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 	
 	public static double roundOne(double num) {
 		num = Math.round(num * 10);
@@ -135,7 +156,7 @@ public class MouseDrawingMain extends Application {
 //		double mouseUpX = 0.0;
 //		double mouseDownY = 0.0;
 //		double mouseUpY = 9.0;
-		graphicContext.strokeLine(mouseDownX, 10, 200, 200);
+		//graphicContext.strokeLine(mouseDownX, 10, 200, 200);
 		
 	}
 
